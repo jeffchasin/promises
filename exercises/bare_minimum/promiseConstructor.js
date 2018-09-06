1d386aa19969a5322dbc7db953d3776b1b8d1228
Jeff Chasin <jeff@jeffchasin.com>
2018-09-05 22:51:45 -0400

    implement getStatusCodeAsync

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -28,6 +28,15 @@ var pluckFirstLineFromFileAsync = function (filePath) {
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function (url) {
   // TODO
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
